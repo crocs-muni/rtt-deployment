12833d66b553cbcea3401806e515c77de877fa07
LuboO <lubomir.obratil@gmail.com>
2017-03-13 11:50:29 +0100

    Dashes in filenames and default paths replaced by underscores Configuration file and constants changed accordingly.

--- common/rtt_constants.py
+++ common/rtt_constants.py
@@ -4,10 +4,10 @@
 
 class CommonConst(object):
     COMMON_FILES_DIR = "../common"
-    CREATE_TABLES_SCRIPT = "db-files/create-rtt-tables.sql"
-    STORAGE_CLEAN_CACHE = "storage-files/clean_cache.py"
-    FRONTEND_SUBMIT_EXPERIMENT_SCRIPT = "frontend-files/submit_experiment.py"
-    FRONTEND_ADD_USER_SCRIPT = "frontend-files/add_rtt_user.py"
+    CREATE_TABLES_SCRIPT = "db_files/create_rtt_tables.sql"
+    STORAGE_CLEAN_CACHE = "storage_files/clean_cache.py"
+    FRONTEND_SUBMIT_EXPERIMENT_SCRIPT = "frontend_files/submit_experiment.py"
+    FRONTEND_ADD_USER_SCRIPT = "frontend_files/add_rtt_user.py"
 
 
 class Database(object):
@@ -18,35 +18,35 @@ class Database(object):
 
 class Storage(object):
     CHROOT_HOME_DIR = "/home"
-    CHROOT_DATA_DIR = "data-files"
-    CHROOT_CONF_DIR = "config-files"
+    CHROOT_DATA_DIR = "data_files"
+    CHROOT_CONF_DIR = "config_files"
     RTT_ADMIN_GROUP = "rtt_admin"
     SSH_DIR = ".ssh"
     AUTH_KEYS_FILE = "authorized_keys"
     STORE_CONFIG_FILE = "storage.ini"
     CLEAN_CACHE_SCRIPT = "clean_cache.py"
-    CLEAN_CACHE_LOG = "clean-cache.log"
+    CLEAN_CACHE_LOG = "clean_cache.log"
     COMMON_FILES_DIR = "common"
     CREDENTIALS_DIR = "credentials"
     MYSQL_STORAGE_USER = "rtt_storage"
-    MYSQL_CREDENTIALS_FILE = "db-mysql-cred.ini"
+    MYSQL_CREDENTIALS_FILE = "db_mysql_cred.ini"
 
 
 class Frontend(object):
     RTT_ADMIN_GROUP = "rtt_admin"
     RTT_USER_GROUP = "rtt_user"
-    CHROOT_RTT_FILES = "/rtt-files"
+    CHROOT_RTT_FILES = "/rtt_frontend_files"
     CHROOT_RTT_USERS_HOME = "/home"
     FRONT_CONFIG_FILE = "frontend.ini"
     ADD_USER_SCRIPT = "add_rtt_user.py"
     SUBMIT_EXPERIMENT_SCRIPT = "submit_experiment.py"
-    SUBMIT_EXPERIMENT_BINARY = "submit-experiment"
+    SUBMIT_EXPERIMENT_BINARY = "submit_experiment"
     CREDENTIALS_DIR = "credentials"
     COMMON_FILES_DIR = "common"
     MYSQL_FRONTEND_USER = "rtt_frontend"
-    MYSQL_CREDENTIALS_FILE = "db-mysql-cred.ini"
-    SSH_CREDENTIALS_FILE = "storage-ssh-cred.ini"
-    SSH_CREDENTIALS_KEY = "storage-ssh-key"
+    MYSQL_CREDENTIALS_FILE = "db_mysql_cred.ini"
+    SSH_CREDENTIALS_FILE = "storage_ssh_cred.ini"
+    SSH_CREDENTIALS_KEY = "storage_ssh_key"
     SSH_DIR = ".ssh"
     AUTH_KEYS_FILE = "authorized_keys"
     FSTAB_FILE = "/etc/fstab"
