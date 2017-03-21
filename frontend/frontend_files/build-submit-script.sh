1aa7837f0014434632df436cf9cb660c36eb3e42
xobrati1 <lubomir.obratil@gmail.com>
2017-03-21 08:52:34 +0100

    Removed old files

--- frontend/frontend_files/build-submit-script.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-pyinstaller -F submit-experiment.py
-mv dist/submit-experiment .
-chown :rtt-admin submit-experiment
-chmod 2775 submit-experiment
-rm -rf build dist submit-experiment.spec
